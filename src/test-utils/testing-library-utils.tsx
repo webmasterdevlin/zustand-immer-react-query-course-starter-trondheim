940fd6c1057897f374b84a851445fdcf92993158
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-25 15:11:49 +0200

    update

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,6 +1,8 @@
 import { render as rtlRender } from '@testing-library/react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { queryClient } from '../App';
 import FallbackRenderer from '../components/FallbackRenderer';
 import NavigationBar from '../components/NavigationBar';
 import type { ReactElement, ReactNode } from 'react';
@@ -8,14 +10,16 @@ import type { ReactElement, ReactNode } from 'react';
 const render = (ui: ReactElement, { ...renderOptions } = {}) => {
   const wrapper = ({ children }: { children: ReactNode }) => {
     return (
-      <BrowserRouter>
-        <NavigationBar />
-        <ErrorBoundary fallbackRender={FallbackRenderer}>
-          <div className={' bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-white'}>
-            {children}
-          </div>
-        </ErrorBoundary>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <NavigationBar />
+          <ErrorBoundary fallbackRender={FallbackRenderer}>
+            <div className={' bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800 dark:text-white'}>
+              {children}
+            </div>
+          </ErrorBoundary>
+        </BrowserRouter>
+      </QueryClientProvider>
     );
   };
 
