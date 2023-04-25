9d85b20092faf086e67229ba9026acca839d88e9
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-25 12:39:34 +0200

    update

---
+++ src/setupTests.ts
@@ -0,0 +1,13 @@
+import '@testing-library/dom';
+
+beforeAll(() => {
+  console.log('beforeAll');
+});
+
+afterEach(() => {
+  console.log('afterEach');
+});
+
+afterAll(() => {
+  console.log('afterAll');
+});
