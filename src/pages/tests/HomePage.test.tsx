324933f92442d6ed0694c19def7f75195850e910
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-25 13:06:23 +0200

    update

--- src/pages/tests/HomePage.test.tsx
+++ src/pages/tests/HomePage.test.tsx
@@ -8,5 +8,13 @@ describe('Home page', () => {
 
     const welcomeBack = screen.getByTestId('welcome-message');
     expect(welcomeBack).toHaveTextContent(/welcome/i);
+    expect(welcomeBack).toHaveClass('dark:text-white');
+  });
+
+  it('welcome message should be white text', () => {
+    render(<HomePage />);
+
+    const welcomeBack = screen.getByTestId('welcome-message');
+    expect(welcomeBack).toHaveClass('dark:text-white');
   });
 });
