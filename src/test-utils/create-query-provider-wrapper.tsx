4f4f3bdba89677996cb37295e0799d5dbefb32d2
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-25 15:37:22 +0200

    update

---
+++ src/test-utils/create-query-provider-wrapper.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import type { ReactNode } from 'react';
+import type { QueryClientConfig } from 'react-query';
+
+export type QueryProviderWrapperProps = {
+  children: ReactNode;
+};
+
+export const createQueryProviderWrapper = (config: QueryClientConfig = {}) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+    ...config,
+  });
+
+  const QueryComponent = ({ children }: QueryProviderWrapperProps) => {
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  };
+  return QueryComponent;
+};
